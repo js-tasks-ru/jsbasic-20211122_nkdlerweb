2b08cac8cad1236b1144082b1f8cd762edcf25b1
SchHeinz <nkdlerweb@gmail.com>
2021-11-24 17:57:12 +0200

    Added function in script.js

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,11 @@
-function sum(m, n) {
-  // ваш код...
+function sum() {
+  var result = 0;
+
+  for (var i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+
+  return result;
 }
+alert( sum(1,1) );
+alert( sum(2,5) );
