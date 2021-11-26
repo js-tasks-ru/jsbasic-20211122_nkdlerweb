3dffa4324c6f61ed32f6569201e029d10c48ef4c
SchHeinz <nkdlerweb@gmail.com>
2021-11-26 13:28:27 +0200

    Fixed function in index.js for factorial = 0

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,5 +1,8 @@
 function factorial(n) {
             result = n;
+			if (n === 0) {
+				result = 1;
+			}
             for (let i = n - 1; i > 1; --i) {
                 result *= i;
             }
