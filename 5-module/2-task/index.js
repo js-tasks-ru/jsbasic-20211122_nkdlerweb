5648729dc59e8d9a74fa68b518b567f5a84c530f
SchHeinz <nkdlerweb@gmail.com>
2021-12-10 11:35:07 +0200

    complete module-5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+  let textToggle = document.querySelector('.toggle-text-button');
+  const text = document.getElementById('text');
+  
+  textToggle.addEventListener('click', () => {
+
+    text.hidden = !text.hidden;
+    
+  })
 }
